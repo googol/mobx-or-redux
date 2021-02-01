32871054ecc216d6b6c3f0ad957325ea5b115b64
Miika Hänninen <miika.hanninen@reaktor.com>
2021-02-01 17:03:30 +0200

    Remove mobx bits

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,8 @@ import { ThemeProvider, GlobalStyle } from '@react95/core'
 import '@react95/icons/icons.css'
 import './App.css'
 import { installReduxNotesApp } from './ReduxNotesApp'
-import { installMobXNotesApp } from './MobXNotesApp'
 
 const ReduxNotesApp = installReduxNotesApp()
-const MobXNotesApp = installMobXNotesApp()
 
 function App() {
   return (
@@ -16,10 +14,6 @@ function App() {
         <div>
           <h1>Redux</h1>
           <ReduxNotesApp />
-        </div>
-        <div>
-          <h1>MobX</h1>
-          <MobXNotesApp />
         </div>
       </div>
     </ThemeProvider>
